0b383daba63f0e973bc52695a1d5e274ec797dfe
buddhi549 <humbleandwise16@gmail.com>
2022-02-07 19:54:42 +0530

    started development

--- finalmeetups/apps.py
+++ finalmeetups/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MeetupsConfig(AppConfig):
+class FinalmeetupsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meetups'
+    name = 'finalmeetups'
